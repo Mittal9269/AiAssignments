39edb23d30e360ec6d43f811e641882cfce75766
Mittal9269 <190030026@iitsh.ac.in>
2021-12-25 23:36:55 +0530

    seond_commit

--- assignment1/q1.py
+++ assignment1/q1.py
@@ -4,7 +4,7 @@ from collections import deque
 moveI = [1 , -1 , 0 , 0]
 moveJ = [0 ,  0, 1,  -1]
 
-def isValid(i , j):
+def isValid(i , j ):
     m = len(lines)
     n = len(lines[0])
     if(i >= 0 and i < m and j >= 0 and j < m and lines[i][j] == ' '):
@@ -12,15 +12,26 @@ def isValid(i , j):
     return 0
 
 def bfs():
+    m = len(lines)
+    n = len(lines[0])
     queue = deque([])
     queue.append([0 , 0])
+    # visited = []
+    # for i in range(m):
+    #     visited.append([0]*n)
+    # visited[0][0] = 1
     while(len(queue)):
         temp = queue.popleft()
         if(lines[temp[0]][temp[1]] == "*"):
-            return 
-        for i in range(4):
-            if(isValid(temp[0] + moveI[i] , temp[0] + moveI[i])):
-                queue.append([temp[0] + moveI[i] , temp[0] + moveI[i]])
+            return temp[0],temp[1]
+
+        for k in range(4):
+            i ,j = temp[0] + moveI[k] , temp[1] + moveJ[k]
+            if(isValid(i , j)):
+                queue.append([i , j])
+                lines[i][j] = '+'
+                # visited[temp[0] + moveI[i]][temp[0] + moveI[i]]
+
 
 def dfs():
     pass
@@ -40,11 +51,11 @@ for line in Lines:
     lines.append(line.replace("\n" , ""))
 
 if(intital_number == 0):
-    bfs()
+    print(bfs())
 elif(intital_number == 1):
-    dfs()
+    print(dfs())
 else:
-    dfid()
+    print(dfid())
 
 
 
