87767d41ab562900b828b26f01dafec5b9e7accc
tanjiro23 <ayushgupta232002@gmail.com>
2022-07-20 14:21:33 +0530

    Adding assignment 3

--- assignment3/17.py
+++ assignment3/17.py
@@ -6,11 +6,12 @@ import copy
 from generate import Clause
 from itertools import combinations
 
+
 def printState(stringOfVar):
     string = ""
     for position in range(len(stringOfVar)):
         if stringOfVar[position] == "0":
-            string += "~" +  chr(ord("A") + position) + " "
+            string += "~" + chr(ord("A") + position) + " "
         else:
             string += chr(ord("A") + position) + " "
 
@@ -25,31 +26,30 @@ def rand_key(p):
     return(key1)
 
 
-def Heuristic(Clause , stringOfVar):
+def Heuristic(Clause, stringOfVar):
     state_value = 0
     for PerClause in Clause:
         for literal in PerClause:
             if int(literal) < 0 and stringOfVar[abs(int(literal)) - 1] == '0':
-                state_value += 1 
+                state_value += 1
                 break
             if int(literal) > 0 and stringOfVar[abs(int(literal)) - 1] == '1':
                 state_value += 1
-                break      
+                break
     return -1*state_value
 
 
-
 def nextGenfunction(string, density):
-    neighbour  = []
-    allCombinations=list(combinations(range(len(string)),density))
+    neighbour = []
+    allCombinations = list(combinations(range(len(string)), density))
     for combination in allCombinations:
         new_string = string
         for position in combination:
-            new_string = string[:position] + str(1 - int(new_string[position])) + string[position+1:] 
-        value = Heuristic(Clause , new_string)
+            new_string = string[:position] + \
+                str(1 - int(new_string[position])) + string[position+1:]
+        value = Heuristic(Clause, new_string)
         neighbour.append((value, new_string))
     return neighbour
-    
 
 
 def TebuSearch(StringOfVar):
@@ -68,36 +68,34 @@ def TebuSearch(StringOfVar):
     print("present State " + printState(CandidateOfBestState))
 
     limitChecker = 1
-    totalStateVisited  = 1
+    totalStateVisited = 1
     while (limitChecker < limit and Heuristic(Clause, bestState) != (-1*k)):
         limitChecker += 1
         sNeighborhood = nextGenfunction(CandidateOfBestState, 2)
         CandidateOfBestState = []
         for sCandidate in sNeighborhood:
-            totalStateVisited += 1 
+            totalStateVisited += 1
             if (sCandidate[1] not in tabuList):
                 if len(CandidateOfBestState) == 0:
                     CandidateOfBestState = sCandidate[1]
                 else:
                     if(sCandidate[0] < Heuristic(Clause, CandidateOfBestState)):
                         CandidateOfBestState = sCandidate[1]
-        
+
         if len(CandidateOfBestState) == 0:
-            print("No Futher Improvement possible stuck at local maxima " + printState(bestState))
+            print("No Futher Improvement possible stuck at local maxima " +
+                  printState(bestState))
             print("total Node explored " + str(limitChecker))
             print("total state explored " + str(totalStateVisited))
             return False
         print("present State " + printState(CandidateOfBestState))
 
-        if  (Heuristic(Clause, CandidateOfBestState) < Heuristic(Clause, bestState)):
+        if (Heuristic(Clause, CandidateOfBestState) < Heuristic(Clause, bestState)):
             bestState = CandidateOfBestState
 
         tabuList.append(CandidateOfBestState)
         if (len(tabuList) > maxTabuSize):
             tabuList.popleft()
-
-
-
 
     if(limitChecker >= limit):
         print("Solution don't exist by limit")
@@ -111,31 +109,31 @@ def TebuSearch(StringOfVar):
     return True
 
 
-
-
 def VarNeighbourDescent(stringOfVar):
     visited = []
     visited.append(stringOfVar)
     length_path = 0
-    checkValue = Heuristic(Clause , stringOfVar)
+    checkValue = Heuristic(Clause, stringOfVar)
 
     density = 1
     totalStateVisited = 0
     if(checkValue == (-1*k)):
         print("Success! Number of Nodes explored are " + str(length_path + 1))
-        print("Success! Number of states explored are " + str(totalStateVisited + 1))
+        print("Success! Number of states explored are " +
+              str(totalStateVisited + 1))
         print("Final state " + printState(stringOfVar))
         return True
     while(checkValue != (-1*k) and density <= n):
         if (density == 1):
             length_path += 1
-        checkValue = Heuristic(Clause , stringOfVar)
+        checkValue = Heuristic(Clause, stringOfVar)
         if(checkValue == (-1*k)):
             print("Success! Number of Nodes explored are " + str(length_path + 1))
             print("Number of Nodes explored are " + str(totalStateVisited + 1))
             print("Final State " + printState(stringOfVar))
             return True
-        print("Present state " + printState(stringOfVar) + " with density " + str(density))
+        print("Present state " + printState(stringOfVar) +
+              " with density " + str(density))
 
         next_Gen_neighbours = nextGenfunction(stringOfVar, density)
         Indicator = False
@@ -161,8 +159,10 @@ def VarNeighbourDescent(stringOfVar):
 
         if(density == n):
             print("Stuck at local maxima")
-            print("There is no solution for VND approach and number of Nodes explored are " + str(length_path))
-            print("There is no solution for VND approach and number of states explored are " + str(totalStateVisited))
+            print(
+                "There is no solution for VND approach and number of Nodes explored are " + str(length_path))
+            print("There is no solution for VND approach and number of states explored are " +
+                  str(totalStateVisited))
             print("Stuck at" + printState(stringOfVar))
             return False
 
@@ -170,14 +170,13 @@ def VarNeighbourDescent(stringOfVar):
     return False
 
 
-
-def BeamSearch(stringOfVar , beta):
+def BeamSearch(stringOfVar, beta):
     visited = []
     visited.append(stringOfVar)
     length_path = 0
     checkValue = Heuristic(Clause, stringOfVar)
-    open =  PriorityQueue()
-    open.put((checkValue , stringOfVar))
+    open = PriorityQueue()
+    open.put((checkValue, stringOfVar))
     totalStateVisited = 1
 
     while(not open.empty()):
@@ -187,19 +186,20 @@ def BeamSearch(stringOfVar , beta):
             topElement = open.get()
             if(topElement[0] == -1*k):
                 print("Success! Number of Nodes explored are " + str(length_path))
-                print("Success! Number of states explored are " + str(totalStateVisited))
+                print("Success! Number of states explored are " +
+                      str(totalStateVisited))
                 print("Final state is " + printState(topElement[1]))
                 return True
             print("Present state " + printState(topElement[1]))
 
             next_Gen_neighbours = nextGenfunction(topElement[1], 1)
             for neighbours in next_Gen_neighbours:
-                totalStateVisited += 1 
+                totalStateVisited += 1
                 if neighbours[1] not in visited:
                     visited.append(neighbours[1])
                     if neighbours[0] <= topElement[0]:
                         listOfCandidate.append(neighbours)
-        listOfCandidate = sorted(listOfCandidate , key = lambda  x : (x[0]))
+        listOfCandidate = sorted(listOfCandidate, key=lambda x: (x[0]))
 
         if len(listOfCandidate) >= beta:
             for i in range(beta):
@@ -208,11 +208,11 @@ def BeamSearch(stringOfVar , beta):
             for Candidate in listOfCandidate:
                 open.put(Candidate)
 
-
         if(open.empty()):
             print("Stuck at local maxima " + printState(stringOfVar))
-            print("There is no solution for Beam search approach  and number of Nodes explored are " +str(length_path))
-            print("There is no solution for Beam search approach  and number of states explored are " +str(totalStateVisited))
+            print("There is no solution for Beam search approach  and number of Nodes explored are " + str(length_path))
+            print("There is no solution for Beam search approach  and number of states explored are " +
+                  str(totalStateVisited))
             return False
 
 
@@ -228,7 +228,7 @@ print("intial string " + printState(string))
 print("\n" + "\n" + "\n")
 # print(Heuristic(Clause , string))
 print("Performance of Beam Search")
-print(BeamSearch(string , int(sys.argv[3])))
+print(BeamSearch(string, int(sys.argv[3])))
 print("#########################-----------------------#############---------------------------------------#################--------------")
 print("-------------------------#######################-------------#######################################-----------------##############")
 maxTabuSize = int(sys.argv[4])
--- assignment3/generate.py
+++ assignment3/generate.py
@@ -1,9 +1,10 @@
 import sys
 import random
 
+
 def perClause(n):
     listOfVar = []
-    for i in range(-n , n+1):
+    for i in range(-n, n+1):
         if i == 0:
             continue
         listOfVar.append(i)
@@ -17,11 +18,11 @@ def perClause(n):
 
     return sampleClause
 
-
     # list = random.sample(list, n)
     # pass
 
-def genClauses(n , k):
+
+def genClauses(n, k):
     clauseList = []
     i = 0
     while i < k:
@@ -29,30 +30,31 @@ def genClauses(n , k):
         testClause.sort()
         if testClause not in clauseList:
             clauseList.append(testClause)
-            i += 1 
+            i += 1
     return clauseList
 
     # pass
 
+
 n = int(sys.argv[1])
 k = int(sys.argv[2])
 NumberOfLiteral = 3
-Clause = genClauses(n ,k)
+Clause = genClauses(n, k)
 # print(Clause)
 file = open('input.txt', 'w')
 count = 0
 for PerClause in Clause:
-    string  = ""
+    string = ""
     for element in PerClause:
         if element < 0:
-            string += "~" +  chr(ord("A") + abs(element) - 1) + " "
+            string += "~" + chr(ord("a") + abs(element) - 1) + " "
         else:
-            string +=  chr(ord("A") + abs(element) - 1) + " "
-    string  = string[:len(string) - 1]
-    
+            string += chr(ord("a") + abs(element) - 1) + " "
+    string = string[:len(string) - 1]
+
     if (count == len(Clause) - 1):
         file.write(string)
     else:
         file.write(string + '\n')
-    count += 1 
+    count += 1
 file.close()
